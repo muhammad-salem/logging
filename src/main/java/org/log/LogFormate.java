5389a7d7821df6bb9628d9c7b8f1b03a2f1bbbb0
salemebo <salem.ebo@gmail.com>
2019-09-25 02:26:17 +0200

    delete files

--- src/main/java/org/log/LogFormate.java
+++
@@ -1,7 +0,0 @@
-package org.log;
-
-public interface LogFormate {
-	void log(Level level, Class<?> classname, String title, String message);
-	
-	
-}
--- src/main/java/org/log/core/LogFormate.java
+++
@@ -1,9 +0,0 @@
-package org.log.core;
-
-import org.log.Level;
-
-public interface LogFormate {
-	void log(Level level, Class<?> classname, String title, String message);
-	
-	
-}
